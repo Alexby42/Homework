d97f73032f86fcfe5431474a693d267549782a77
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-03-21 14:30:50 +0700

    Add files via upload

--- Project_for.py
+++ Project_for.py
@@ -1,8 +1,6 @@
 list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-for i in range(len(list_)):
-	print('Я езжу на автомобиле марки ' + list_[i])
-print()
 cars_count = 0
 for i in range(len(list_)):
+	print('Я езжу на автомобиле марки ' + list_[i])
 	cars_count +=10
 	print(cars_count)
