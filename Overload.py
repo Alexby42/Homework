83309a04c7c274859ffaf32af3bf5fb0ca8f87c8
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-04-15 15:47:29 +0700

    Update Overload.py

--- Overload.py
+++ Overload.py
@@ -1,12 +1,12 @@
 class Building:
-    def __init__(self, str='one'):
-        self.numberOfFloors = 1
+    def __init__(self, str=None):
+        self.numberOfFloors = 0
         self.buildingType = str
     def __eq__(self, other):
-        return self.numberOfFloors == self.buildingType
-number_ = Building()
-type_ = Building()
-if number_ == type_:
-    print('Целочисленное равно строковому параметру')
-else:
-    print('Целочисленное не равно строковому параметру')
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
+number1 = Building(7)
+number2 = Building(7)
+type1 = Building('seven')
+type2 = Building('twenty four')
+print(number1 == number2)
+print(type1 == type2)
