5c728361f742388a8940a89ada92baa0a2b26c15
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-04-16 10:38:07 +0700

    Update Atr_&_meth_obj.py

--- Atr_&_meth_obj.py
+++ Atr_&_meth_obj.py
@@ -1,6 +1,6 @@
 class House:
     def __init__(self):
         self.numberOfFloors = 10
-        for i in range(self.numberOfFloors):
-            print('Текущий этаж равен:', i + 1)
 house = House()
+for i in range(house.numberOfFloors):
+    print('Текущий этаж равен:', i + 1)
