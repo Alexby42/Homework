fb52bc55a6d9ec7d2721a49f971ad4b8863f36f7
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-04-19 12:03:30 +0700

    Add files via upload

---
+++ Attr_diff.py
@@ -0,0 +1,13 @@
+class Building:
+    total = 0
+
+    def __init__(self):
+        Building.total += 1
+        print(Building.total)
+
+list_ = []
+building_total = 40
+while len(list_) < building_total:
+    building = Building()
+    list_.append(building)
+#print(Building.total)
