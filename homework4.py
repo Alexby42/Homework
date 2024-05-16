e543276850001a03ebb0e138cfa13a0d09d9d990
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-05-16 11:29:49 +0700

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,9 +1,8 @@
-immutable_var = tuple([1, 5.5, True, 'Python'])
-print(immutable_var)
-immutable_var[0] = 10
-print(immutable_var)    # кортеж не поддерживает обращение (назначение) по элементам
-mutable_list = ([1, 5], False, 2.3)
-print(mutable_list)
-mutable_list[0][1] = 8
-print(mutable_list)
-print((mutable_list) + (10, 30))
+immutable_var = tuple([1, 5.5, True, 'Python'])     # переменная с кортежем из различных типов элементов
+print(immutable_var)                                # вывод кортежа
+#immutable_var[0] = 10                              # попытка изменения элемента кортежа
+#print(immutable_var)                               # кортеж не поддерживает обращение (назначение) по элементам
+mutable_list = [1, 5, False, 2.3]                   # переменная со списком из различных типов элементов
+print(mutable_list)                                 # вывод списка переменной
+mutable_list[3] = 'Python'                          # изменение элемента списка
+print(mutable_list)                                 # вывод измененного списка
