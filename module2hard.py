deac0677893ba2efe11a43b5f7f546a8ccaf5fd2
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-05-17 19:32:58 +0700

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -4,7 +4,7 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 print(n)
 for i in range(0, len(numbers)):
     for j in range(i + 1, len(numbers)):
-        if (numbers[i] + numbers[j]) == n: #and (numbers[i] + numbers[j]) % n == 0:
+        if n % (numbers[i] + numbers[j]) == 0:
             summa = numbers[i], numbers[j]
             result = ''.join(map(str, summa))
             print(result, end="")
