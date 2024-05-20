056e71d0cc9725881305e6b7d33b4ae848b6f7b1
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-05-20 23:22:42 +0700

    Update Class_inherit.py

--- Class_inherit.py
+++ Class_inherit.py
@@ -18,23 +18,3 @@ auto2.Horse_powers(300)
 auto3 = Kia()
 print(auto3)
 auto3.Horse_powers(400)
-
-print('--------------------Первый вариант, получившийся на основе лекций--------------------')
-
-class Car:
-    price = 1000000
-    def __init__(self, Horse_powers):
-        self.Horse_powers = Horse_powers
-    def __str__(self):
-        return 'Автомобиль {}, Стоимость: {}, Кол-во лошадиных сил: {}'.format(
-            self.__class__.__name__, self.price, self.Horse_powers)
-class Nissan(Car):
-    price = 2000000
-class Kia(Car):
-    price = 3000000
-auto1 = Car(200)
-print(auto1)
-auto2 = Nissan(300)
-print(auto2)
-auto3 = Kia(400)
-print(auto3)
