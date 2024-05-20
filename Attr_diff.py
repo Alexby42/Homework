ca9fd11c44b90d8f98c53b45e3771eb7af5e2408
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-05-20 13:10:32 +0700

    Update Attr_diff.py

--- Attr_diff.py
+++ Attr_diff.py
@@ -1,13 +1,10 @@
 class Building:
     total = 0
-
     def __init__(self):
         Building.total += 1
         print(Building.total)
-
 list_ = []
 building_total = 40
 while len(list_) < building_total:
     building = Building()
     list_.append(building)
-#print(Building.total)
