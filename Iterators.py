b040d372a6b81b9dac8ba38f40b84e3a9ae01fb0
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-05-22 09:43:29 +0700

    Add files via upload

---
+++ Iterators.py
@@ -0,0 +1,16 @@
+class EvenNumbers:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.start >= self.end:
+            raise StopIteration
+        else:
+            self.start +=2
+        return self.start - 2
+en = EvenNumbers(10, 25)
+print('После перебора и вывода:')
+for i in en:
+        print(i)
