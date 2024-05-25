70498ac77da003d06d9cfc1a6b5249bd95cf3492
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-05-25 12:44:25 +0700

    Update Spec_meth_class.py

--- Spec_meth_class.py
+++ Spec_meth_class.py
@@ -1,18 +1,18 @@
 class House:
     def __init__(self):
         self.numberOfFloors = 0
-    def setNewNumberOfFloors(self):
-        self.numberOfFloors = 5
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
         print(house.numberOfFloors)
 house = House()
-house.setNewNumberOfFloors()
+house.setNewNumberOfFloors(5)
 print()
 class House:
     def __init__(self):
         self.numberOfFloors = 0
-    def setNewNumberOfFloors(self):
-        self.numberOfFloors = (1, 2, 3, 4, 5)
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
         for house.numberOfFloors in self.numberOfFloors:
             print(house.numberOfFloors)
 house = House()
-house.setNewNumberOfFloors()
+house.setNewNumberOfFloors((1, 2, 3, 4, 5))
