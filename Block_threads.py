5097f1c109b8dfb5957447687886fc1032748c21
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-06-19 16:56:45 +0700

    Update Block_threads.py

--- Block_threads.py
+++ Block_threads.py
@@ -4,22 +4,15 @@ class BankAccount:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.deposit = 1000
-        #self.withdraw = withdraw
-        #self.amount = 0
-        #self.account = account
-        #self.amount = amount
     def deposit_task(self, account, amount):
         with lock:
             for _ in range(5):
                 self.deposit += amount
-                #account.deposit(amount)
                 print(f'Deposited {amount}, new balance is {account.deposit}')
     def withdraw_task(self, account, amount):
         with lock:
             for _ in range(5):
                 self.deposit -= amount
-                #account.withdraw(amount)
-                #account = BankAccount()
                 print(f'Withdraw {amount}, new balance is {account.deposit}')
 account = BankAccount()
 deposit_thread = threading.Thread(target=account.deposit_task, args=(account, 100))
