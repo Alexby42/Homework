1412f5aca5081c89d2d1b27c2debd38db2657db4
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-08-01 11:34:11 +0700

    Update tests_12_2.py

--- tests_12_2/tests_12_2.py
+++ tests_12_2/tests_12_2.py
@@ -16,24 +16,19 @@ class Test(unittest.TestCase):
             for key, value in test_value.items():
                 print(f'{key}: {value.name}')
     def test_turn1(self):
-        turn1 = rt.Tournament(100, self.student1, self.student3)
+        turn1 = rt.Tournament(90, self.student1, self.student3)
         result = turn1.start()
-        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Ник всегда должен быть последним')
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ник всегда должен быть последним')
         self.all_results['test_turn1'] = result
     def test_turn2(self):
-        turn2 = rt.Tournament(100, self.student2, self.student3)
+        turn2 = rt.Tournament(90, self.student2, self.student3)
         result = turn2.start()
-        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Ник всегда должен быть последним')
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ник всегда должен быть последним')
         self.all_results['test_turn2'] = result
     def test_turn3(self):
-        turn3 = rt.Tournament(100, self.student1, self.student2, self.student3)
+        turn3 = rt.Tournament(90, self.student1, self.student2, self.student3)
         result = turn3.start()
-        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Ник всегда должен быть последним')
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ник всегда должен быть последним')
         self.all_results['test_turn3'] = result
-    def test_turn4(self):
-        turn_4 = rt.Tournament(10, self.student1, self.student2, self.student3)
-        result = turn_4.start()
-        self.assertTrue(result[list(result.keys())[-1]] == 'Ник', 'Ошибка! Ник всегда должен быть последним')
-        self.all_results['test_turn4'] = result
 if __name__ == '__main__':
     unittest.main()
