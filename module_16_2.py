07026405dea136e52caa36bec03e94d320e27153
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-08-08 10:38:23 +0700

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -8,12 +8,12 @@ async def GetRootPage():
 async def GetAdminPage():
     return 'Вы вошли как администратор'
 @app.get('/user/{userID}')
-async def GetUserID(userID: Annotated[int, Path(ge=1, le=100, description='Enter your id', example='43')]):
+async def GetUserID(userID: Annotated[int, Path(ge=1, le=100, description='Enter your id', examples='43')]):
     return f'Вы вошли как пользователь №{userID}'
 @app.get('/user/{username}/{age}')
 async def GetUserInfo(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
-                                                   example='UrbanUser')], age: Annotated[int, Path(ge=18, le=120,
+                                                   examples='UrbanUser')], age: Annotated[int, Path(ge=18, le=120,
                                                                                                   description=
                                                                                                   'Enter age',
-                                                                                                  example='24')]):
+                                                                                                  examples='24')]):
     return f'Информация о пользователе. Имя {username}, возраст {age}'
