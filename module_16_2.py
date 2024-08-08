d2f9376c19e7ba9c9e73bcd0a10ccf342db7a279
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-08-08 10:26:03 +0700

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+app = FastAPI()
+@app.get('/')
+async def GetRootPage():
+    return 'Главная страница'
+@app.get('/user/admin')
+async def GetAdminPage():
+    return 'Вы вошли как администратор'
+@app.get('/user/{userID}')
+async def GetUserID(userID: Annotated[int, Path(ge=1, le=100, description='Enter your id', example='43')]):
+    return f'Вы вошли как пользователь №{userID}'
+@app.get('/user/{username}/{age}')
+async def GetUserInfo(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+                                                   example='UrbanUser')], age: Annotated[int, Path(ge=18, le=120,
+                                                                                                  description=
+                                                                                                  'Enter age',
+                                                                                                  example='24')]):
+    return f'Информация о пользователе. Имя {username}, возраст {age}'
